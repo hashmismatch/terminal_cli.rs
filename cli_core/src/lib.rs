df1597595e7ea271806fc101e790de89f8b021c8
Rudi Benkovic <rudi.benkovic@gmail.com>
2017-10-23 19:42:52 +0200

    rework 1

--- cli_core/src/lib.rs
+++ cli_core/src/lib.rs
@@ -41,24 +41,20 @@
 //!	
 //! ```
 
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 
-#![cfg_attr(target_os="none", feature(alloc))]
-#![cfg_attr(target_os="none", feature(collections))]
-#![cfg_attr(target_os="none", feature(core_intrinsics))]
-#![cfg_attr(target_os="none", feature(slice_concat_ext))]
+#![cfg_attr(not(feature="std"), feature(alloc))]
+#![cfg_attr(not(feature="std"), feature(collections))]
+#![cfg_attr(not(feature="std"), feature(core_intrinsics))]
+#![cfg_attr(not(feature="std"), feature(slice_concat_ext))] 
 
-#[cfg(any(target_os="none"))]
+#[cfg(not(feature="std"))]
 #[macro_use]
 extern crate alloc;
 
-#[cfg(any(target_os="none"))]
+#[cfg(not(feature="std"))]
 #[macro_use]
 extern crate collections;
-
-#[cfg(any(feature="debug_std", test, not(target_os="none")))]
-#[macro_use]
-extern crate std;
 
 
 mod autocomplete;
@@ -71,7 +67,6 @@ mod cli_property;
 mod keys;
 mod keys_terminal;
 mod terminal;
-mod terminal_telnet;
 mod prompt_buffer;
 
 
@@ -84,13 +79,7 @@ pub use keys::*;
 pub use keys_terminal::*;
 pub use property::*;
 pub use terminal::*;
-pub use terminal_telnet::*;
 pub use prompt_buffer::*;
-
-#[cfg(feature = "termios_support")]
-pub mod terminal_termios;
-
-pub mod terminal_termion;
 
 #[cfg(test)]
 mod tests;
